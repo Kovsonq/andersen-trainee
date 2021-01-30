ac8fa423f8f2404f037db55e88f3e7493d984ed3
Kovsonq <kovsonq@gmail.com>
2021-01-31 02:14:30 +0300

    add 1: web.xml, changed folder structure for web add 2: tomcat add 3: create template good-list.jsp add 4: create template for servlet

---
+++ src/main/java/part2/Servlets/GoodListServlet.java
@@ -0,0 +1,16 @@
+package part2.Servlets;
+
+import lombok.SneakyThrows;
+
+import java.io.IOException;
+
+public class GoodListServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    @SneakyThrows
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+}
---
+++ web/good-list.jsp
@@ -0,0 +1,20 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: kovso
+  Date: 30.01.2021
+  Time: 18:36
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Good list</title>
+</head>
+<body>
+<h1>Good List</h1>
+<p>
+
+</p>
+
+</body>
+</html>
